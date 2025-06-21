ebf6039618d520923b5e5773ccb96b768fce2d8e
unknown <utkarsh chaudhary>
2025-06-21 13:26:45 +0530

    Initial Commit

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Footer = () => {
+  return (
+    <div className='md:mx-10'>
+      <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10  mt-40 text-sm'>
+
+        <div>
+          <img className='mb-5 w-40' src={assets.logo} alt="" />
+          <p className='w-full md:w-2/3 text-gray-600 leading-6'>Prescripto  is your reliable companion for managing healthcare needs with ease and efficiency. Our platform connects patients with a wide network of verified doctors across multiple specialties, allowing you to browse profiles, check availability, and book appointments—all from the comfort of your home.  Prescripto ensures a smooth and stress-free healthcare experience. Whether you need a routine check-up or a specialist consultation, Prescripto is here to make quality healthcare just a click away. </p>
+        </div>
+
+        <div>
+          <p className='text-xl font-medium mb-5'>COMPANY</p>
+          <ul className='flex flex-col gap-2 text-gray-600'>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery</li>
+            <li>Privacy policy</li>
+          </ul>
+        </div>
+
+        <div>
+          <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+          <ul className='flex flex-col gap-2 text-gray-600'>
+            <li>+91 8299254630</li>
+            <li>prescriptocontact@prescripto.com</li>
+          </ul>
+        </div>
+
+      </div>
+
+      <div>
+        <hr />
+        <p className='py-5 text-sm text-center'>Copyright 2025 @ Prescripto.com - All Right Reserved.</p>
+      </div>
+
+    </div>
+  )
+}
+
+export default Footer
